0ce5c877403612add67b70593613b691612c3cb6
BananaSorcery <trantrunghaupro2013@gmail.com>
2022-12-29 14:18:38 +0700

    test old deploy version

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// const app = require('express')();
+const app = require('express')();
 
-// app.get('/', (req, res) =>
-//     //res.json({ message:'Hello World!' })
-//     res.send(<h1>"Hello World!"</h1>)
-// );
+app.get('/', (req, res) =>
+    //res.json({ message:'Hello World!' })
+    res.send(<h1>"Hello World!"</h1>)
+);
 
-// const port = process.env.PORT || 8080;
-
-// app.listen(port,() => {
-//     console.log(`Example app is working on port ${port}`);
-// })
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-// router.get('/about',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
-// });
-
-// router.get('/sitemap',function(req,res){
-//   res.sendFile(path.join(__dirname+'/sitemap.html'));
-// });
-
-//add the router
-// app.use('/', router);
-app.listen(process.env.port || 8080);
+const port = process.env.PORT || 8080;
 
 app.listen(port,() => {
     console.log(`Example app is working on port ${port}`);
 })
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const router = express.Router();
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+// // router.get('/about',function(req,res){
+// //   res.sendFile(path.join(__dirname+'/about.html'));
+// // });
+
+// // router.get('/sitemap',function(req,res){
+// //   res.sendFile(path.join(__dirname+'/sitemap.html'));
+// // });
+
+// //add the router
+// // app.use('/', router);
+// app.listen(process.env.port || 8080);
+
+// app.listen(port,() => {
+//     console.log(`Example app is working on port ${port}`);
+// })
