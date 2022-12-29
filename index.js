2d2228e0b3039d7e2568c93a2c0f6a498afad6b0
BananaSorcery <trantrunghaupro2013@gmail.com>
2022-12-29 16:47:32 +0700

    fix html & css to load the page

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const app = require('express')();
+// const app = require('express')();
 
-app.get('/', (req, res) =>
-    //res.json({ message:'Hello World!' })
-    res.send('<h1>Simple CI/CD project using: Docker, Jenkins (CI) and Azure App Service (CD)</h1>')
-);
+// app.get('/', (req, res) =>
+//     //res.json({ message:'Hello World!' })
+//     res.send('<h1>Simple CI/CD project using: Docker, Jenkins (CI) and Azure App Service (CD)</h1>')
+// );
 
-const port = process.env.PORT || 8080;
-
-app.listen(port,() => {
-    console.log(`App is working on port ${port}`);
-})
-
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
 // const port = process.env.PORT || 8080;
 
-// // sendFile will go here
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, '/views/index.html'));
-// });
-
 // app.listen(port,() => {
-//     console.log(`Example app is working on port ${port}`);
-//     console.log((path.join(__dirname, '/views/index.html')));
+//     console.log(`App is working on port ${port}`);
 // })
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//Set folder to load css, images, js...
+app.use(express.static(__dirname + '/public')); 
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.listen(port,() => {
+    console.log(`Example app is working on port ${port}`);
+})
